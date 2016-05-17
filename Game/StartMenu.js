b6e55dc79db0720a963f1ed54f656d871f47f42e
MaxwellPoster <jellyl0ver1@yahoo.com>
2016-05-17 09:12:50 -0400

    Startmenu functions

--- Game/StartMenu.js
+++ Game/StartMenu.js
@@ -9,9 +9,6 @@ BattleWatch.StartMenu.prototype={
     create: function(){
         this.stage.backgroundColor = '#00FFFF'
         startTitle = this.add.image(0,0,'titleimage');
-        startCredits =this.add.button(705,475,'Credit',game.state.start('Credit'));
-        gamestart =this.add.button(705,315,'Start',game.state.start('Game'));
-        startOption =this.add.button(705,395,'Option',game.state.start('Option'));
         gamestart.inputEnabled = true;
         startOption.inputEnabled = true;
         startCredits.inputEnabled = true;
